34fbf152b66a89b6a236ab644674d44fb7917d76
Harold Yang <harold.h.yang@gmail.com>
2017-09-17 15:44:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay (musicians, instruments) {
   var array = [];
-  for (let i = 0; i < 4; i++) {
-    array.push(`${musicians[0,1,2,3]} plays ${instruments[0,1,2,3]}`)
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " instruments[i])
   }
   return array;
 }
